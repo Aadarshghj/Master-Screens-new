67b4ce8b811d5fc0e330f897ccc10f84c9207a4c
Aadarsh <aadarshaadarsh109@gmail.com>
2026-03-03 14:45:41 +0530

    updated mapping

---
+++ src/types/customer-management/sub-module-management-type.ts
@@ -0,0 +1,14 @@
+export interface SubModule {
+  module: string;
+  subModuleCode: string;
+  subModuleName: string;
+  subModuleDescription: string;
+  isActive: boolean;
+}
+export interface SubModuleTypeDto {
+  module: string;
+  subModuleCode: string;
+  subModuleName: string;
+  subModuleDescription: string;
+  isActive: boolean;
+}
