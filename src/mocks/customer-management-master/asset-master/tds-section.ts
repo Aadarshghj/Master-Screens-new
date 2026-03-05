8931f542b19e7efc08206412765a56e71ba4d83e
Christeena <christeena3002@gmail.com>
2026-03-05 15:53:20 +0530

    tds-model-terms asset updated

---
+++ src/mocks/customer-management-master/asset-master/tds-section.ts
@@ -0,0 +1,15 @@
+import type { TdsSectionTypes } from "@/types/customer-management/asset-master/tds-section";
+
+export const TDS_SECTION_SAMPLE_DATA:TdsSectionTypes[]=[
+    {
+    tdsSectionType:"194Q-Purchase of Goods",
+    description:"----------",
+    isActive:true
+    },
+    {
+    tdsSectionType:"194C-Contractors",
+    description:"----------",
+    isActive:true
+    },
+    
+]
---
+++ src/pages/customer-management/asset-master/tds-section/constants/TdsSectionDefault.ts
@@ -0,0 +1,7 @@
+import type { TdsSectionTypes } from "@/types/customer-management/asset-master/tds-section";
+
+export const TDS_SECTION_DEFAULT_VALUES:TdsSectionTypes={
+     tdsSectionType:"",
+    description:"",
+    isActive:true
+}
